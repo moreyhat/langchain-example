af9dfe290f91917f8593501fc91f5dce65dd2477
moreyhat <moreyhat@gmail.com>
2023-12-12 20:11:30 -0800

    Add cache for conversation history

--- caching/app.py
+++ caching/app.py
@@ -1,12 +1,43 @@
 from langchain.globals import set_llm_cache
 from langchain.cache import RedisSemanticCache
+from langchain.memory import ConversationBufferMemory
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from operator import itemgetter
 
-llm = OpenAI()
+
 set_llm_cache(RedisSemanticCache(redis_url="redis://localhost:6379", embedding=OpenAIEmbeddings()))
 
-res = llm("Tell me a joke")
-print(res)
-res = llm("Tell me a joke")
-print(res)
+llm = ChatOpenAI()
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful chatbot"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{input}"),
+    ]
+)
+
+memory = ConversationBufferMemory(return_messages=True)
+memory.load_memory_variables({})
+
+chain = (
+    RunnablePassthrough.assign(
+        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+    )
+    | prompt
+    | llm
+)
+
+inputs = {"input": "Hi I'm bob"}
+response = chain.invoke(inputs)
+
+memory.save_context(inputs, {"output": response.content})
+print(memory.load_memory_variables({}))
+
+inputs = {"input": "Whats my name"}
+response = chain.invoke(inputs)
+print(response.content)
+
+print(response.content)
