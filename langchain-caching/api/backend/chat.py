bb8586fe0d9a3a50f5955fe0f1468b23b44424fb
moreyhat <moreyhat@gmail.com>
2023-12-22 23:11:22 -0800

    Change Cache DB to AstraDB and embeddings to Hugging Face

--- langchain-caching/api/backend/chat.py
+++ langchain-caching/api/backend/chat.py
@@ -3,19 +3,32 @@ from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.globals import set_llm_cache
-from langchain.cache import RedisSemanticCache
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.cache import AstraDBSemanticCache
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+import os
 
-set_llm_cache(
-    RedisSemanticCache(
-        redis_url="redis://cache:6379",
-        embedding=OpenAIEmbeddings()
-        ),
+HUGGING_FACE_API_KEY = os.environ["HUGGING_FACE_API_KEY"]
+ASTRADB_ENDPOINT = os.environ["ASTRADB_ENDPOINT"]
+ASTRADB_TOKEN = os.environ["ASTRADB_TOKEN"]
+
+embedding = HuggingFaceInferenceAPIEmbeddings(
+    api_key=HUGGING_FACE_API_KEY,
+    model_name="pkshatech/GLuCoSE-base-ja",
 )
+
+cache_db = AstraDBSemanticCache(
+    api_endpoint=ASTRADB_ENDPOINT,
+    token=ASTRADB_TOKEN,
+    embedding=embedding,
+)
+
+set_llm_cache(cache_db)
+
 
 class ConversationMessage(BaseModel):
     type: str
     content: str
+
 
 class ChatRequest(BaseModel):
     message: str
@@ -23,10 +36,10 @@ class ChatRequest(BaseModel):
     cache: bool = False
 
 
-class Chat():
+class Chat:
     def __init__(self) -> None:
         self.client = ChatOpenAI(cache=False)
-    
+
     def enable_cache(self):
         self.client.cache = True
 
@@ -40,8 +53,8 @@ class Chat():
                 messages.append(SystemMessage(content=conversation_msg.content))
             else:
                 messages.append(HumanMessage(content=conversation_msg.content))
-        
+
         messages.append(HumanMessage(content=message))
         response = self.client(messages)
-        
+
         return response.content
